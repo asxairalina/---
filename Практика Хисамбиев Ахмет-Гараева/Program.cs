719e650dbb108e079f2062d0a4957554728c4409
212227 <212227@1220-012-22>
2023-10-23 10:05:25 +0300

    Добавьте файлы проекта.

---
+++ Практика Хисамбиев Ахмет-Гараева/Program.cs
@@ -0,0 +1,24 @@
+﻿int a;
+int d;
+
+while (true)
+{
+    Console.WriteLine("Введите сумму");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите скидку");
+    d = Convert.ToInt32(Console.ReadLine());
+
+    if (a >= 1 && a <= 100000 && d >= 1 && d <= 100)
+    {
+        double c = a - (a * d) / 100;
+        Console.WriteLine(c);
+        break;
+
+    }
+
+    else
+    {
+        Console.WriteLine("Неправильные данные");   
+    }
+}
+
